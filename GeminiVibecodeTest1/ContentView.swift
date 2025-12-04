a3c16d4be0ca201f4e04d2eac555b8b3c60c3a8e
Jeremy Trafas <jeremytrafas@gmail.com>
2025-12-03 20:07:48 -0600

    Initial Commit

---
+++ GeminiVibecodeTest1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GeminiVibecodeTest1
+//
+//  Created by user287057 on 12/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GeminiVibecodeTest1/GeminiVibecodeTest1App.swift
@@ -0,0 +1,17 @@
+//
+//  GeminiVibecodeTest1App.swift
+//  GeminiVibecodeTest1
+//
+//  Created by user287057 on 12/3/25.
+//
+
+import SwiftUI
+
+@main
+struct GeminiVibecodeTest1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
